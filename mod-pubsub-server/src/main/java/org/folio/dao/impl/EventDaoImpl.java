2bca5299a24060247433a3cb68d8a734367418a1
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2019-08-09 17:47:21 +0300

    Use stub impl for event dao

--- mod-pubsub-server/src/main/java/org/folio/dao/impl/EventDaoImpl.java
+++ mod-pubsub-server/src/main/java/org/folio/dao/impl/EventDaoImpl.java
@@ -1,13 +1,9 @@
 package org.folio.dao.impl;
 
 import io.vertx.core.Future;
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.sql.ResultSet;
 import org.folio.dao.EventDao;
 import org.folio.dao.PostgresClientFactory;
 import org.folio.rest.jaxrs.model.Event;
-import org.folio.rest.persist.PostgresClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -16,34 +12,16 @@ import java.util.Optional;
 @Repository
 public class EventDaoImpl implements EventDao {
 
-  private static final String TABLE_NAME = "events";
-  private static final String GET_BY_ID_QUERY = "SELECT id FROM %s.%s WHERE id = ?";
-
   @Autowired
-  private PostgresClientFactory pgClientFactory;
+  private PostgresClientFactory pgClientFactory; // NOSONAR
 
   @Override
   public Future<Optional<Event>> getById(String eventId, String tenantId) {
-    Future<ResultSet> future = Future.future();
-    try {
-      String query = String.format(GET_BY_ID_QUERY, PostgresClient.convertToPsqlStandard(tenantId), TABLE_NAME);
-      JsonArray params = new JsonArray().add(eventId);
-      pgClientFactory.getInstance(tenantId).select(query, params, future.completer());
-    } catch (Exception e) {
-      future.fail(e);
-    }
-    return future.map(resultSet -> {
-      if (resultSet.getResults().isEmpty()) {
-        return Optional.empty();
-      } else {
-        return Optional.of(mapToEvent(resultSet.getRows().get(0)));
-      }
-    });
-  }
-
-  private Event mapToEvent(JsonObject jsonObject) {
-    // Override mapping right after Event schema confirmed
-    return new Event()
-      .withId(jsonObject.getString("id"));
+    /*
+      Implementation is stubbed.
+      Use pgClientFactory to obtain PostgresClient instance and
+      query for Event entity from the underlying database.
+    */
+    return Future.succeededFuture(Optional.of(new Event().withId(eventId)));
   }
 }
