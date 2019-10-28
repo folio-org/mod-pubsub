e2b3ff3f15e05238d4e50787f36400cfb52709ca
Kateryna Senchenko <kateryna_senchenko@epam.com>
2019-10-28 17:02:37 +0200

    Configured embedded kafka

--- mod-pubsub-server/src/test/java/org/folio/spring/config/TestConfig.java
+++ mod-pubsub-server/src/test/java/org/folio/spring/config/TestConfig.java
@@ -1,9 +1,7 @@
 package org.folio.spring.config;
 
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 
 @Import(ApplicationConfig.class)
-@PropertySource("classpath:test-application.properties")
 public class TestConfig {
 }
