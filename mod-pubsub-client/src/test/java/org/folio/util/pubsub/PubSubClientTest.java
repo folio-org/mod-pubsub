b87ae948d636c197f2e69f84897e1c79463788d9
oleksii_kuzminov <oleksii_kuzminov@epam.com>
2019-11-19 15:54:43 +0300

    MODPUBSUB-47 Create pub-sub client for module registration and message sending

--- mod-pubsub-client/src/test/java/org/folio/util/pubsub/PubSubClientTest.java
+++ mod-pubsub-client/src/test/java/org/folio/util/pubsub/PubSubClientTest.java
@@ -3,10 +3,13 @@ package org.folio.util.pubsub;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.folio.rest.jaxrs.model.Event;
+import org.folio.rest.jaxrs.model.EventMetadata;
 import org.folio.util.pubsub.support.OkapiConnectionParams;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.UUID;
 
 @RunWith(VertxUnitRunner.class)
 public class PubSubClientTest extends AbstractRestTest {
@@ -23,6 +26,17 @@ public class PubSubClientTest extends AbstractRestTest {
   @Test
   public void RegisterModuleSuccessfully(TestContext context) throws Exception {
     context.assertTrue(PubSubClientUtils.registerModule(params).get());
+  }
+
+  @Test
+  public void sendMessageEventSuccessfully(TestContext context) throws Exception {
+    context.assertTrue(PubSubClientUtils.sendEventMessage(new Event()
+        .withId(UUID.randomUUID().toString())
+        .withEventType("CREATED_SRS_MARC_BIB_RECORD_WITH_ORDER_DATA")
+        .withEventMetadata(new EventMetadata()
+          .withEventTTL(1)
+          .withTenantId(TENANT_ID)),
+      params).get());
   }
 
 }
