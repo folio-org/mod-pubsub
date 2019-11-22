c1607835e498503fc4e52ff8e007b6d956434c6c
Kateryna Senchenko <kateryna_senchenko@epam.com>
2019-11-22 15:40:38 +0200

    Created Security Manager

--- mod-pubsub-server/src/main/java/org/folio/rest/util/OkapiConnectionParams.java
+++ mod-pubsub-server/src/main/java/org/folio/rest/util/OkapiConnectionParams.java
@@ -1,25 +1,31 @@
 package org.folio.rest.util;
 
+import io.vertx.core.Vertx;
+
 import java.util.Map;
 
 public final class OkapiConnectionParams {
 
-  private static final String OKAPI_URL_HEADER = "x-okapi-url";
-  private static final String OKAPI_TENANT_HEADER = "x-okapi-tenant";
-  private static final String OKAPI_TOKEN_HEADER = "x-okapi-token";
+  public static final String OKAPI_URL_HEADER = "x-okapi-url";
+  public static final String OKAPI_TENANT_HEADER = "x-okapi-tenant";
+  public static final String OKAPI_TOKEN_HEADER = "x-okapi-token";
   private String okapiUrl;
   private String tenantId;
   private String token;
   private Map<String, String> headers;
+  private Vertx vertx;
+  private int timeout = 2000;
 
-  public OkapiConnectionParams() {
+  public OkapiConnectionParams(Vertx vertx) {
+    this.vertx = vertx;
   }
 
-  public OkapiConnectionParams(Map<String, String> okapiHeaders) {
+  public OkapiConnectionParams(Map<String, String> okapiHeaders, Vertx vertx) {
     this.okapiUrl = okapiHeaders.getOrDefault(OKAPI_URL_HEADER, "localhost");
     this.tenantId = okapiHeaders.getOrDefault(OKAPI_TENANT_HEADER, "");
     this.token = okapiHeaders.getOrDefault(OKAPI_TOKEN_HEADER, "dummy");
     this.headers = okapiHeaders;
+    this.vertx = vertx;
   }
 
   public String getOkapiUrl() {
@@ -27,6 +33,7 @@ public final class OkapiConnectionParams {
   }
 
   public void setOkapiUrl(String okapiUrl) {
+    headers.put(OKAPI_URL_HEADER, okapiUrl);
     this.okapiUrl = okapiUrl;
   }
 
@@ -35,6 +42,7 @@ public final class OkapiConnectionParams {
   }
 
   public void setTenantId(String tenantId) {
+    headers.put(OKAPI_TENANT_HEADER, tenantId);
     this.tenantId = tenantId;
   }
 
@@ -43,6 +51,7 @@ public final class OkapiConnectionParams {
   }
 
   public void setToken(String token) {
+    headers.put(OKAPI_TOKEN_HEADER, token);
     this.token = token;
   }
 
@@ -51,6 +60,25 @@ public final class OkapiConnectionParams {
   }
 
   public void setHeaders(Map<String, String> headers) {
+    this.okapiUrl = headers.getOrDefault(OKAPI_URL_HEADER, "localhost");
+    this.tenantId = headers.getOrDefault(OKAPI_TENANT_HEADER, "");
+    this.token = headers.getOrDefault(OKAPI_TOKEN_HEADER, "dummy");
     this.headers = headers;
+  }
+
+  public Vertx getVertx() {
+    return vertx;
+  }
+
+  public void setVertx(Vertx vertx) {
+    this.vertx = vertx;
+  }
+
+  public int getTimeout() {
+    return timeout;
+  }
+
+  public void setTimeout(int timeout) {
+    this.timeout = timeout;
   }
 }
