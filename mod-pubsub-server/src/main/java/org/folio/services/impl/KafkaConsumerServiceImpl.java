f1819d1981e294b5c73cd8762fdd5fc85589a5b3
VRohach <Volodymyr_Rohach@epam.com>
2020-02-14 11:36:30 +0200

    MODPUBSUB-66: Handle 'IndexOutOfBoundsException' when mod-pubsub try to register new modules. 1. Logging changed to debug level.

--- mod-pubsub-server/src/main/java/org/folio/services/impl/KafkaConsumerServiceImpl.java
+++ mod-pubsub-server/src/main/java/org/folio/services/impl/KafkaConsumerServiceImpl.java
@@ -108,7 +108,7 @@ public class KafkaConsumerServiceImpl implements ConsumerService {
           messagingModuleList
             .forEach(subscriber -> doRequest(event.getEventPayload(), subscriber.getSubscriberCallback(), HttpMethod.POST, params)
               .setHandler(getEventDeliveredHandler(event, params.getTenantId(), subscriber)));
-          LOGGER.info("Event with id: {} delivered", event.getId());
+          LOGGER.debug("Event with id: {} delivered", event.getId());
         }
         return Future.succeededFuture();
       });
