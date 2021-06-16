188a06e9969eb62504c0939dece5f51acd83b2ae
VRohach <Volodymyr_Rohach@epam.com>
2021-06-16 16:57:47 +0300

    MODPUBSUB-182: Provide properties for Kafka security in mod-pubsub

--- mod-pubsub-server/src/main/java/org/folio/kafka/SpringKafkaProperties.java
+++ mod-pubsub-server/src/main/java/org/folio/kafka/SpringKafkaProperties.java
@@ -1,6 +1,6 @@
 package org.folio.kafka;
 
-public final class SpringKafkaProperties {  //NOSONAR
+public final class SpringKafkaProperties {
 
   public static final String KAFKA_SECURITY_PROTOCOL = "spring.kafka.security.protocol";
 
