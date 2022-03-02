8b6f004e07054b4ecf3291cc2675e236345750fa
Yusuf Murodov <yusuf_murodov@epam.com>
2022-03-02 11:50:48 +0500

    buitified comments

--- mod-pubsub-server/src/main/java/org/folio/services/ConsumerService.java
+++ mod-pubsub-server/src/main/java/org/folio/services/ConsumerService.java
@@ -15,7 +15,7 @@ public interface ConsumerService {
    *
    * @param eventTypes list of event types that specified module is subscribing to receive
    * @param params     Okapi connection params
-   * @return succeeded Future
+   * @return a future completed with a result
    */
   Future<Void> subscribe(List<String> eventTypes, OkapiConnectionParams params);
 }
