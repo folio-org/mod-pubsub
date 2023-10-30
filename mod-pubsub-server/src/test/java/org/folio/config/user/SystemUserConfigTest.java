d851b32ff503d1155764a1060b710d8d9b6aefd6
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2023-10-30 12:01:17 +0200

    MODPUBSUB-278 Rename test method

--- mod-pubsub-server/src/test/java/org/folio/config/user/SystemUserConfigTest.java
+++ mod-pubsub-server/src/test/java/org/folio/config/user/SystemUserConfigTest.java
@@ -22,24 +22,26 @@ public class SystemUserConfigTest {
 
   @Test
   public void invalidSystemUserCredentials() {
-    testValidation(VALID_USERNAME, null, SYSTEM_USER_PASSWORD_VAR);
-    testValidation(VALID_USERNAME, EMPTY_STRING, SYSTEM_USER_PASSWORD_VAR);
-    testValidation(VALID_USERNAME, BLANK_STRING, SYSTEM_USER_PASSWORD_VAR);
-    testValidation(null, VALID_PASSWORD, SYSTEM_USER_NAME_VAR);
-    testValidation(null, null, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
-    testValidation(null, EMPTY_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
-    testValidation(null, BLANK_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
-    testValidation(EMPTY_STRING, VALID_PASSWORD, SYSTEM_USER_NAME_VAR);
-    testValidation(EMPTY_STRING, null, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
-    testValidation(EMPTY_STRING, EMPTY_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
-    testValidation(EMPTY_STRING, BLANK_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
-    testValidation(BLANK_STRING, VALID_PASSWORD, SYSTEM_USER_NAME_VAR);
-    testValidation(BLANK_STRING, null, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
-    testValidation(BLANK_STRING, EMPTY_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
-    testValidation(BLANK_STRING, BLANK_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testFailedValidation(VALID_USERNAME, null, SYSTEM_USER_PASSWORD_VAR);
+    testFailedValidation(VALID_USERNAME, EMPTY_STRING, SYSTEM_USER_PASSWORD_VAR);
+    testFailedValidation(VALID_USERNAME, BLANK_STRING, SYSTEM_USER_PASSWORD_VAR);
+    testFailedValidation(null, VALID_PASSWORD, SYSTEM_USER_NAME_VAR);
+    testFailedValidation(null, null, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testFailedValidation(null, EMPTY_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testFailedValidation(null, BLANK_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testFailedValidation(EMPTY_STRING, VALID_PASSWORD, SYSTEM_USER_NAME_VAR);
+    testFailedValidation(EMPTY_STRING, null, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testFailedValidation(EMPTY_STRING, EMPTY_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testFailedValidation(EMPTY_STRING, BLANK_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testFailedValidation(BLANK_STRING, VALID_PASSWORD, SYSTEM_USER_NAME_VAR);
+    testFailedValidation(BLANK_STRING, null, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testFailedValidation(BLANK_STRING, EMPTY_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testFailedValidation(BLANK_STRING, BLANK_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
   }
 
-  private static void testValidation(String username, String password, String... missingVariables) {
+  private static void testFailedValidation(String username, String password,
+    String... missingVariables) {
+
     try {
       new SystemUserConfig(username, password);
     } catch (IllegalArgumentException e) {
