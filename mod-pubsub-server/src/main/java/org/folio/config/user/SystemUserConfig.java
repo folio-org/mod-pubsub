36487ab23a7b07f343f70368cf8709d6cb42cd6c
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2023-10-30 12:00:01 +0200

    MODPUBSUB-278 Extend validation, add more tests

--- mod-pubsub-server/src/main/java/org/folio/config/user/SystemUserConfig.java
+++ mod-pubsub-server/src/main/java/org/folio/config/user/SystemUserConfig.java
@@ -1,7 +1,10 @@
 package org.folio.config.user;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -35,24 +38,30 @@ public class SystemUserConfig {
       .put("password", password);
   }
 
-  private static void validateCredentials(String name, String password) {
+  private static void validateCredentials(String username, String password) {
     LOGGER.info("validateCredentials:: validating system user credentials");
     List<String> missingVariables = new ArrayList<>();
-    if (name == null) {
-      LOGGER.error("validateCredentials:: username is null");
+    if (isBlank(username)) {
+      LOGGER.error("validateCredentials:: username={}", quotedValueOrNull(username));
       missingVariables.add(SYSTEM_USER_NAME_VAR);
     }
-    if (password == null) {
-      LOGGER.error("validateCredentials:: password is null");
+    if (isBlank(password)) {
+      LOGGER.error("validateCredentials:: password={}", quotedValueOrNull(password));
       missingVariables.add(SYSTEM_USER_PASSWORD_VAR);
     }
     if (!missingVariables.isEmpty()) {
-      String errorMessage = "Failed to resolve credentials for system user. " +
-        "Please provide missing system variables: " + missingVariables;
+      String errorMessage = "Invalid system user credentials. " +
+        "Please provide non-blank values for system variables: " + missingVariables;
       LOGGER.error("validateCredentials:: {}", errorMessage);
       throw new IllegalArgumentException(errorMessage);
     }
     LOGGER.info("validateCredentials:: system user credentials are valid");
+  }
+
+  private static String quotedValueOrNull(String s) {
+    return Optional.ofNullable(s)
+      .map(str -> "\"" + str + "\"")
+      .orElse(null);
   }
 
 }
--- mod-pubsub-server/src/test/java/org/folio/config/user/SystemUserConfigTest.java
+++ mod-pubsub-server/src/test/java/org/folio/config/user/SystemUserConfigTest.java
@@ -8,30 +8,46 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class SystemUserConfigTest {
+  private static final String SYSTEM_USER_NAME_VAR = "SYSTEM_USER_NAME";
+  private static final String SYSTEM_USER_PASSWORD_VAR = "SYSTEM_USER_PASSWORD";
+  private static final String VALID_USERNAME = "test-username";
+  private static final String VALID_PASSWORD = "test-password";
+  private static final String EMPTY_STRING = "";
+  private static final String BLANK_STRING = "   ";
 
-  @Test(expected = IllegalArgumentException.class)
-  public void constructionFailsWhenUsernameAndPasswordAreNull() {
-    testValidation(null, null, "SYSTEM_USER_NAME", "SYSTEM_USER_PASSWORD");
+  @Test
+  public void validSystemUserCredentials() {
+    new SystemUserConfig(VALID_USERNAME, VALID_PASSWORD);
   }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void constructionFailsWhenUsernameIsNull() {
-    testValidation(null, "testPassword", "SYSTEM_USER_NAME");
+  @Test
+  public void invalidSystemUserCredentials() {
+    testValidation(VALID_USERNAME, null, SYSTEM_USER_PASSWORD_VAR);
+    testValidation(VALID_USERNAME, EMPTY_STRING, SYSTEM_USER_PASSWORD_VAR);
+    testValidation(VALID_USERNAME, BLANK_STRING, SYSTEM_USER_PASSWORD_VAR);
+    testValidation(null, VALID_PASSWORD, SYSTEM_USER_NAME_VAR);
+    testValidation(null, null, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testValidation(null, EMPTY_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testValidation(null, BLANK_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testValidation(EMPTY_STRING, VALID_PASSWORD, SYSTEM_USER_NAME_VAR);
+    testValidation(EMPTY_STRING, null, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testValidation(EMPTY_STRING, EMPTY_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testValidation(EMPTY_STRING, BLANK_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testValidation(BLANK_STRING, VALID_PASSWORD, SYSTEM_USER_NAME_VAR);
+    testValidation(BLANK_STRING, null, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testValidation(BLANK_STRING, EMPTY_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
+    testValidation(BLANK_STRING, BLANK_STRING, SYSTEM_USER_NAME_VAR, SYSTEM_USER_PASSWORD_VAR);
   }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void constructionFailsWhenPasswordIsNull() {
-    testValidation("test_user", null, "SYSTEM_USER_PASSWORD");
-  }
-
-  private void testValidation(String username, String password, String... missingVariables) {
+  private static void testValidation(String username, String password, String... missingVariables) {
     try {
       new SystemUserConfig(username, password);
     } catch (IllegalArgumentException e) {
-      assertThat(e.getMessage(), is("Failed to resolve credentials for system user. " +
-        "Please provide missing system variables: " + Arrays.toString(missingVariables)));
-      throw e;
+      assertThat(e.getMessage(), is("Invalid system user credentials. " +
+        "Please provide non-blank values for system variables: " + Arrays.toString(missingVariables)));
+      return;
     }
+    throw new AssertionError("IllegalArgumentException was expected");
   }
 
 }
