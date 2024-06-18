ee580b2af15992d6ef020bc5a74096bf45f580d9
Yauhen Vavilkin <yauhen_vavilkin@epam.com>
2024-06-18 19:46:36 +0400

    EUREKA-79: revert logging

--- mod-pubsub-server/src/main/java/org/folio/services/impl/KafkaConsumerServiceImpl.java
+++ mod-pubsub-server/src/main/java/org/folio/services/impl/KafkaConsumerServiceImpl.java
@@ -179,7 +179,7 @@ public class KafkaConsumerServiceImpl implements ConsumerService {
           && statusCode != HttpStatus.HTTP_NO_CONTENT.toInt()) {
 
           String error = format("Error delivering %s event with id '%s' to %s, response status code is %s, %s",
-            event.getEventType(), event.getId(), subscriber.getSubscriberCallback(), statusCode, ar.result().getResponse().bodyAsString());
+            event.getEventType(), event.getId(), subscriber.getSubscriberCallback(), statusCode, ar.result().getResponse().statusMessage());
           LOGGER.error(error);
           auditService.saveAuditMessage(constructJsonAuditMessage(event, tenantId, AuditMessage.State.REJECTED, error));
           if (statusCode >= 400 && statusCode < 500) {
