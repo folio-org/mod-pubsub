afda11edae08234fc16c07ad4c0c59281d7887f0
alexanderkurash <alexander.kurash@gmail.com>
2025-12-12 12:01:56 +0200

    MODPUBSUB-333 Wait for delete

--- mod-pubsub-server/src/test/java/org/folio/rest/impl/AuditMessageAPITest.java
+++ mod-pubsub-server/src/test/java/org/folio/rest/impl/AuditMessageAPITest.java
@@ -42,9 +42,10 @@ public class AuditMessageAPITest extends AbstractRestTest {
   AutoCloseable openMocks;
 
   @BeforeEach
-  public void setUp() {
+  public void setUp(VertxTestContext testContext) {
     postgresClientFactory.getInstance().execute("DELETE FROM %s.%s"
-      .formatted(convertToPsqlStandard(TENANT_ID), "audit_message_payload"));
+      .formatted(convertToPsqlStandard(TENANT_ID), "audit_message_payload"))
+      .onSuccess(r -> testContext.completeNow());
     openMocks = MockitoAnnotations.openMocks(this);
   }
 
